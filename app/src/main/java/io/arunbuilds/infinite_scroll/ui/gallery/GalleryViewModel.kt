d61371cfd34b7f7ee715171c2ba34454e55a8d7c
Arun Sudharsan <sudarun@amazon.com>
2021-01-05 19:55:30 +0530

    Handling Process Death - MVVM Image Search App with Architecture Components & Retrofit #13

--- app/src/main/java/io/arunbuilds/infinite_scroll/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/io/arunbuilds/infinite_scroll/ui/gallery/GalleryViewModel.kt
@@ -1,7 +1,8 @@
 package io.arunbuilds.infinite_scroll.ui.gallery
 
+import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
@@ -9,9 +10,10 @@ import androidx.paging.cachedIn
 import io.arunbuilds.infinite_scroll.data.UnsplashRepository
 
 class GalleryViewModel @ViewModelInject constructor(
-    private val repository: UnsplashRepository
+    private val repository: UnsplashRepository,
+    @Assisted state: SavedStateHandle
 ) : ViewModel() {
-    private val currentQuery = MutableLiveData<String>(DEFAULT_QUERY)
+    private val currentQuery = state.getLiveData<String>(KEY_CURRENT_QUERY, DEFAULT_QUERY)
     val photos = currentQuery.switchMap { query ->
         repository.getSearchResults(query).cachedIn(viewModelScope)
     }
@@ -22,5 +24,6 @@ class GalleryViewModel @ViewModelInject constructor(
 
     companion object {
         private const val DEFAULT_QUERY = "Spiderman"
+        private const val KEY_CURRENT_QUERY = "current_query"
     }
 }
