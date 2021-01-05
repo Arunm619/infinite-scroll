a372ac8a0f9bd70a00811f27e5b58dab96f0197f
Arun Sudharsan <sudarun@amazon.com>
2021-01-05 15:41:32 +0530

    Layouts & Model Class - MVVM Image Search App with Architecture Components & Retrofit #2

---
+++ app/src/main/java/io/arunbuilds/infinite_scroll/UnsplashPhoto.kt
@@ -0,0 +1,30 @@
+package io.arunbuilds.infinite_scroll
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class UnsplashPhoto(
+    val id: String,
+    val description: String?,
+    val urls: UnsplashPhotoUrls,
+    val user: UnsplashUser
+) : Parcelable {
+    @Parcelize
+    data class UnsplashPhotoUrls(
+        val raw: String,
+        val full: String,
+        val regular: String,
+        val small: String,
+        val thumb: String
+    ) : Parcelable
+
+    @Parcelize
+    data class UnsplashUser(
+        val name: String,
+        val username: String
+    ) : Parcelable {
+        val attributionUrl: String
+            get() = "https://unsplash.com/$username?utm_source=infinitescroll&utm_medium=referral"
+    }
+}
